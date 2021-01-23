1bd7fa7c144ae586140c6cb7130a8f5d5c2a36a0
Alisson Corrales <Acorra435@west-mec.local>
2021-01-22 18:27:08 -0700

    finished css, starting javascript 1/22/2021

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+class User{
+    constructor(name, DOB, ID){
+        this.name = name;
+        this.DOB = DOB;
+        this.ID = ID;
+    }
+}
+
+
+let ID = 1987;
+let userList = [("Yolanda Pauling", "1/12/1990", 1986), ("Mickard Mundy", "2, 25, 2001", 1985)];
+
+function submitInfo(){
+    return
+}
