5ad02e3594573cc9a02105b1413a4e720e0fe675
Alisson Corrales <Acorra435@west-mec.local>
2021-01-25 18:19:13 -0700

    replaced html and css with a base. actually started javascript 1/25/2021

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
 "use strict";
 
+//this holds all the info you give
 class User{
-    constructor(name, DOB, ID){
-        this.name = name;
-        this.DOB = DOB;
-        this.ID = ID;
+    constructor(firstName, lastName, DoB, departing, arriving, leaveDate, returnDate, chick, fish, veg, legroom, window, headphones, moreFood){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.DoB = DoB;
+        this.departing = departing;
+        this.arriving = arriving;
+        this.leaveDate = leaveDate;
+        this.returnDate = returnDate;
+        this.bags = bags;
+        this.chick = chick;
+        this.fish = fish;
+        this.veg = veg;
+        this.legroom = legroom;
+        this.window = window;
+        this.headphones = headphones;
+        this.moreFood = moreFood;
     }
 }
 
 
 let ID = 1987;
-let userList = [("Yolanda Pauling", "1/12/1990", 1986), ("Mickard Mundy", "2, 25, 2001", 1985)];
+//array for holding values
+let userList = [("Ludwig", "Van Pelt", "1/12/1890", 1986, ""), ("Mickard", "Mundy", "2/25/2001", 1935, "")]; //array with users
 
-function submitInfo(){
-    return
+//this adds the inputted info
+function addToList(){
+    let firstName = document.getElementById("firstName").value; //this takes the value in the input and puts it in the new user
+    let lastName = document.getElementById("lastName").value;
+    let DoB = document.getElementById("DoB").value;
+    let departing = document.getElementById("departing").value;
+    let arriving = document.getElementById("arriving").value;
+    let leaveDate = document.getElementById("leaveDate").value;
+    let returnDate = document.getElementById("returnDate").value;
+    let bags = document.getElementById("bags").value;
+    let chick = document.getElementById("chick").value;
+    let fish = document.getElementById("fish").value;
+    let veg = document.getElementById("veg").value;
+    let legroom = document.getElementById("legroom").value;
+    let window = document.getElementById("window").value;
+
+    let passenger = new User(firstName, lastName, DoB, departing, arriving, leaveDate, returnDate, bags, chick, fish, veg, legroom, window); //the new user
+
+    document.getElementById("firstName").value = ""; //this makes the place where you input empty
+    document.getElementById("lastName").value = "";
+    document.getElementById("DoB").value = "";
+    document.getElementById("departing").value = "";
+    document.getElementById("arriving").value = "";
+    document.getElementById("leaveDate").value = "";
+    document.getElementById("returnDate").value = "";
+    document.getElementById("bags").value = "";
+    document.getElementById("chick").value = "";
+    document.getElementById("fish").value = "";
+    document.getElementById("veg").value = "";
+    document.getElementById("legroom").value = "";
+    document.getElementById("window").value = "";
 }
